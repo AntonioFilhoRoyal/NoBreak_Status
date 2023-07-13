327b77a7bdc10d2b3bf0a1c09c046609f8f9e70e
Antonio Filho <antoniofilhocastro001@gmail.com>
2023-07-12 22:29:13 -0300

    Insertion of equipment in the list and improvement in stylization

--- assets/js/program.js
+++ assets/js/program.js
@@ -1,6 +1,41 @@
 console.log("Estou aqui");
+const inputEquipament = document.querySelector('.input-equipment');
+const inputConsumption = document.querySelector('.input-consumption');
+const areaConsumption = document.querySelector('.area-consumption');
+const ulList = document.querySelector('.list-equipament');
 
 const buttonAddList = document.querySelector('.button-add-nobreak')
 .addEventListener('click', e => {
     e.preventDefault();
+
+    //if(!inputEquipament.value && !inputConsumption.value) return;
+    listNobreak(inputEquipament.value, inputConsumption.value);
+    consumptionNobreak(inputConsumption.value);
 })
+
+function createdLi(){
+    const li = document.createElement("li");
+    li.classList.add("item-list");
+    li.classList.add("font-public-sans");
+    return li;
+}
+
+function listNobreak(equipament, consumption){
+    const li = createdLi();
+    li.innerText = `${equipament} - ${consumption}V`;
+    ulList.appendChild(li);
+    clearInput();
+}
+
+function clearInput(){
+    inputEquipament.value = " ";
+    inputConsumption.value = " ";
+    inputEquipament.focus();
+    inputConsumption.focus();
+}
+
+function consumptionNobreak(consumption){
+    const currentConsumption = consumption;
+    const maxConsumtion = 500;
+    areaConsumption.innerHTML =`${currentConsumption}/${maxConsumtion}`;
+} 
